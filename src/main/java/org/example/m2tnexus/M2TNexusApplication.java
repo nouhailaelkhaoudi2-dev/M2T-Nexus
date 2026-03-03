c3c5b589145d294c5cd8e8b28afc85dc508ba250
nouhailaelkhaoudi2-dev <nouhaila.elkhaoudi2@gmail.com>
2026-03-03 13:21:02 +0000

    Initial commit

---
+++ src/main/java/org/example/m2tnexus/M2TNexusApplication.java
@@ -0,0 +1,13 @@
+package org.example.m2tnexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2TNexusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M2TNexusApplication.class, args);
+    }
+
+}
